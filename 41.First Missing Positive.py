874571635a0d435051b1922b35ed8750779e590b
Mahalakshmi J <165121768+maha1505@users.noreply.github.com>
2025-02-11 10:05:49 +0530

    Add solution for First Missing Positive (LeetCode 41) in python

---
+++ 41.First Missing Positive.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums.sort()       
+        i = 1             
+        for num in nums:
+            if num==i:  
+                i += 1
+            elif num>i: 
+                break
+        return i
