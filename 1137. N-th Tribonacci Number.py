c08a668cbd21efb22156385a74af56f84b9b198c
Mahalakshmi J <165121768+maha1505@users.noreply.github.com>
2025-03-05 16:21:42 +0530

    Update 1137. N-th Tribonacci Number.py

--- 1137. N-th Tribonacci Number.py
+++ 1137. N-th Tribonacci Number.py
@@ -1,8 +1,10 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n<=1:
-            return n
+        if n==0:
+            return 0
+        if n==1 or n==2:
+            return 1
         a,b,c=0,1,1
-        for i in range(2,n):
+        for i in range(3,n+1):
             a,b,c=b,c,a+b+c
         return c
