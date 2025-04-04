6038c46e499787ba0a38d4fbe6f8cefd4e24bd27
Mahalakshmi J <165121768+maha1505@users.noreply.github.com>
2025-04-04 09:25:06 +0530

    Create 0042. Trapping Rain Water.c

---
+++ 0042. Trapping Rain Water.c
@@ -0,0 +1,21 @@
+int trap(int* height, int heightSize) {
+    int l=0,r=heightSize-1;
+    int lmax=0,rmax=0,sum=0;
+    while(l<=r){
+        if(height[l]<height[r]){
+            if(lmax<height[l]){
+                lmax=height[l];
+            }
+            sum+=lmax-height[l];
+            l++;
+        }
+        else{
+            if(rmax<height[r]){
+                rmax=height[r];
+            }
+            sum+=rmax-height[r];
+            r--;
+        }
+    }
+    return sum;
+}
