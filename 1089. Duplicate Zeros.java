d9c887a9f83400bd5efd7ae519921e94e4edf34b
Mahalakshmi J <165121768+maha1505@users.noreply.github.com>
2025-04-05 09:57:17 +0530

    Create 1089. Duplicate Zeros.java

---
+++ 1089. Duplicate Zeros.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int arrSize=arr.length;
+        for(int i=0;i<arrSize;i++){
+            if(arr[i]==0){
+                for(int j=arrSize-1;j>i+1;j--){
+                    arr[j]=arr[j-1];
+                }
+                if(i+1<arrSize)
+                arr[i+1]=0;
+                i+=1;
+            }
+        }
+    }
+}
