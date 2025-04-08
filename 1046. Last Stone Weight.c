82858f582ebe3ba8c48fb4ef1ecdc21fb4269602
Mahalakshmi J <165121768+maha1505@users.noreply.github.com>
2025-04-08 21:46:12 +0530

    Create 1046. Last Stone Weight.c

---
+++ 1046. Last Stone Weight.c
@@ -0,0 +1,19 @@
+int lastStoneWeight(int* stones, int stonesSize) {
+    while(stonesSize>1){
+        for(int i=0;i<stonesSize;i++){
+            for(int j=0;j<stonesSize-1;j++)
+            {
+                if(stones[j]>stones[j+1]){
+                    int temp=stones[j];
+                    stones[j]=stones[j+1];
+                    stones[j+1]=temp;
+                }
+            }
+        }
+        int m=stones[stonesSize-1];
+        int n=stones[stonesSize-2];
+        stones[stonesSize-2]=m-n;
+        stonesSize-=1;
+    }
+    return stones[0];
+}
