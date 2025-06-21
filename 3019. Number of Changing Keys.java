0f5200dbd135bff3b46d18d8900c9f323d9b36ec
Mahalakshmi J <165121768+maha1505@users.noreply.github.com>
2025-06-21 06:21:55 +0530

    Update 3019. Number of Changing Keys.java

--- 3019. Number of Changing Keys.java
+++ 3019. Number of Changing Keys.java
@@ -8,3 +8,13 @@ class Solution {
         return count;
     }
 }
+// class Solution {
+//     public int countKeyChanges(String s) {
+//         String s1=s.toLowerCase();
+//         int count=0;
+//         for(int i=0;i<s1.length()-1;i++){
+//             if(s1.charAt(i)!=s1.charAt(i+1)) count++;
+//         }
+//         return count;
+//     }
+// }
