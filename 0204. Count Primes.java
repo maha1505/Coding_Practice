cd032286b6aaad18e3ec06257fea01821c0890c2
Mahalakshmi J <165121768+maha1505@users.noreply.github.com>
2025-06-25 11:53:15 +0530

    Update 0204. Count Primes.java

--- 0204. Count Primes.java
+++ 0204. Count Primes.java
@@ -1,4 +1,24 @@
 class Solution {
+    public int countPrimes(int n) {		
+        if(n<3) return 0;
+        boolean[] arr=new boolean[n];
+        Arrays.fill(arr,true);
+        int c=n/2;
+        for(int i=3;i*i<n;i+=2){
+            if(arr[i]){
+                for(int j=i*i;j<n;j+=2*i){
+                    if(arr[j]){
+                    arr[j]=false;
+                    c--;
+                    }
+                }
+            }
+        }
+        return c;
+    }
+}
+/*
+class Solution {
     public int countPrimes(int n) {
         int c=0;		
         boolean[] arr=new boolean[n+1];
@@ -16,3 +36,4 @@ class Solution {
         return c;
     }
 }
+*/
