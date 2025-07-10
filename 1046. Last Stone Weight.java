899796ea9488b06d4d906da77f8004a85c358d44
Mahalakshmi J <165121768+maha1505@users.noreply.github.com>
2025-07-10 14:38:51 +0530

    Update 1046. Last Stone Weight.java

--- 1046. Last Stone Weight.java
+++ 1046. Last Stone Weight.java
@@ -2,16 +2,17 @@ class Solution {
     public int lastStoneWeight(int[] stones) {
         int stonesSize=stones.length;
         while(stonesSize>1){
-            for(int i=0;i<stonesSize;i++){
-                for(int j=0;j<stonesSize-1;j++)
-                {
-                    if(stones[j]>stones[j+1]){
-                        int temp=stones[j];
-                        stones[j]=stones[j+1];
-                        stones[j+1]=temp;
-                    }
-                }
-            }
+            // for(int i=0;i<stonesSize;i++){
+            //     for(int j=0;j<stonesSize-1;j++)
+            //     {
+            //         if(stones[j]>stones[j+1]){
+            //             int temp=stones[j];
+            //             stones[j]=stones[j+1];
+            //             stones[j+1]=temp;
+            //         }
+            //     }
+            // }
+            Arrays.sort(stones);
             int m=stones[stonesSize-1];
             int n=stones[stonesSize-2];
             stones[stonesSize-2]=m-n;
